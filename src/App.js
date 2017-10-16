ab914e6e3292610b7e2ad1c7314050c7e6d7829b
Jake Partusch <jakepartusch@gmail.com>
2017-10-15 22:37:22 -0500

    Adding Power Rankings Grid

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import { FantasyFootballApi } from './api/FantasyFootballApi';
 import UrlEntryModal from './components/UrlEntryModal';
-import { Segment, Table, Header, Image, Grid, Container, Form, Button, Loader, Modal, Icon, Message } from 'semantic-ui-react';
+import PowerRankingsGrid from './components/PowerRankingsGrid';
+import { Container, Loader } from 'semantic-ui-react';
 import '../node_modules/semantic-ui-css/semantic.min.css';
-import { getParams } from './util/utils'
 
 class App extends Component {
 
@@ -51,49 +51,10 @@ class App extends Component {
         />
         <Loader active={this.state.loading}/>
         <Container>
-        <Grid centered>
-          <Grid.Row>
-          </Grid.Row>
-          {this.state.rankings.length > 0 && <Grid.Row>
-          <Grid.Column computer={8} tablet={8} mobile={16}>
-            <a href="#" onClick={this.handleOpen}>Switch to a different League</a>
-            <Segment>
-              <Header>Power Rankings 2017</Header>
-              <Table basic='very' celled unstackable>
-                <Table.Header>
-                  <Table.Row>
-                    <Table.HeaderCell>Team</Table.HeaderCell>
-                    <Table.HeaderCell>Wins</Table.HeaderCell>
-                    <Table.HeaderCell>Losses</Table.HeaderCell>
-                  </Table.Row>
-                </Table.Header>
-
-                <Table.Body>
-                  { this.state.rankings.map(team =>
-                  <Table.Row>
-                    <Table.Cell>
-                      <Header as='h4' image>
-                        <Image src={team.logoUrl} shape='rounded' size='mini' />
-                        <Header.Content>
-                            {team.name}
-                          <Header.Subheader>{team.owner}</Header.Subheader>
-                        </Header.Content>
-                      </Header>
-                    </Table.Cell>
-                    <Table.Cell>
-                        {team.wins}
-                    </Table.Cell>
-                    <Table.Cell>
-                        {team.losses}
-                    </Table.Cell>
-                  </Table.Row>
-                  )}
-                </Table.Body>
-              </Table>
-            </Segment>
-          </Grid.Column>
-          </Grid.Row>}
-        </Grid>
+          <PowerRankingsGrid
+            handleOpen={this.handleOpen.bind(this)}
+            rankings={this.state.rankings}
+          />
         </Container>
       </div>
     );
