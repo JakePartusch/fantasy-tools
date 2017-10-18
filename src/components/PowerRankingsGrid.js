fa8895b286b070411e740e336a573f94d94db66c
Jake Partusch <jakepartusch@gmail.com>
2017-10-17 22:41:07 -0500

    Switching to use react-router (#20)

--- src/components/PowerRankingsGrid.js
+++ src/components/PowerRankingsGrid.js
@@ -1,60 +1,90 @@
 import React, { Component } from 'react';
 import { func, arrayOf, object } from 'prop-types';
-import { Grid, Segment, Header, Table, Image} from 'semantic-ui-react';
+import { Grid, Segment, Header, Table, Image, Loader } from 'semantic-ui-react';
+import { FantasyFootballApi } from '../api/FantasyFootballApi';
+import { withRouter, Link } from 'react-router-dom';
 
-export default class PowerRankingsGrid extends Component {
-    static propTypes = {
-        handleOpen: func.isRequired,
-        rankings: arrayOf(object).isRequired
-    };
+class PowerRankingsGrid extends Component {
+    static propTypes = {};
     static defaultProps = {};
 
-    render() {
-        const { handleOpen, rankings } = this.props;
-        return (
-            <Grid centered>
-            <Grid.Row>
-            </Grid.Row>
-            {rankings.length > 0 && <Grid.Row>
-            <Grid.Column computer={8} tablet={8} mobile={16}>
-                <a href="#" onClick={handleOpen}>Switch to a different League</a>
-                <Segment>
-                <Header>Power Rankings 2017</Header>
-                <Table basic='very' celled unstackable>
-                    <Table.Header>
-                    <Table.Row>
-                        <Table.HeaderCell>Team</Table.HeaderCell>
-                        <Table.HeaderCell>Wins</Table.HeaderCell>
-                        <Table.HeaderCell>Losses</Table.HeaderCell>
-                    </Table.Row>
-                    </Table.Header>
+    constructor() {
+        super();
+        this.api = new FantasyFootballApi();
+        this.state = {
+            rankings: []
+        }
+    }
 
-                    <Table.Body>
-                    { rankings.map(team =>
-                    <Table.Row>
-                        <Table.Cell>
-                        <Header as='h4' image>
-                            <Image src={team.logoUrl} shape='rounded' size='mini' />
-                            <Header.Content>
-                                {team.name}
-                            <Header.Subheader>{team.owner}</Header.Subheader>
-                            </Header.Content>
-                        </Header>
-                        </Table.Cell>
-                        <Table.Cell>
-                            {team.wins}
-                        </Table.Cell>
-                        <Table.Cell>
-                            {team.losses}
-                        </Table.Cell>
-                    </Table.Row>
-                    )}
-                    </Table.Body>
-                </Table>
-                </Segment>
-            </Grid.Column>
-            </Grid.Row>}
+    async componentWillMount() {
+        this.setState({loading: true})
+        try {
+            let rankings = await this.api.getPowerRankings(this.props.match.params.leagueId, this.props.match.params.seasonId);
+            const userData = await this.api.getUserData(this.props.match.params.leagueId, this.props.match.params.seasonId);
+            rankings = rankings.map(team => ({
+                ...userData.find(user => user.id === team.id),
+                ...team
+            }));
+            this.setState({rankings, loading:false})
+        } catch(e) {
+            this.props.history.push("/error");
+        }
+    }
+
+    switchLeague() {
+        this.props.history.push("/");
+    }
+
+    render() {
+        const { rankings } = this.state;
+        return (
+        <div>
+            <Loader active={this.state.loading}/>
+            <Grid centered>
+                <Grid.Row>
+                </Grid.Row>
+                {rankings.length > 0 && <Grid.Row>
+                <Grid.Column computer={8} tablet={8} mobile={16}>
+                    <Link to="/">Switch to a different League</Link>
+                    <Segment>
+                    <Header>Power Rankings 2017</Header>
+                    <Table basic='very' celled unstackable>
+                        <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell>Team</Table.HeaderCell>
+                            <Table.HeaderCell>Wins</Table.HeaderCell>
+                            <Table.HeaderCell>Losses</Table.HeaderCell>
+                        </Table.Row>
+                        </Table.Header>
+
+                        <Table.Body>
+                        { rankings.map(team =>
+                        <Table.Row>
+                            <Table.Cell>
+                            <Header as='h4' image>
+                                <Image src={team.logoUrl} shape='rounded' size='mini' />
+                                <Header.Content>
+                                    {team.name}
+                                <Header.Subheader>{team.owner}</Header.Subheader>
+                                </Header.Content>
+                            </Header>
+                            </Table.Cell>
+                            <Table.Cell>
+                                {team.wins}
+                            </Table.Cell>
+                            <Table.Cell>
+                                {team.losses}
+                            </Table.Cell>
+                        </Table.Row>
+                        )}
+                        </Table.Body>
+                    </Table>
+                    </Segment>
+                </Grid.Column>
+                </Grid.Row>}
             </Grid>
+            </div>
         )
     }
 }
+export default withRouter(PowerRankingsGrid)
