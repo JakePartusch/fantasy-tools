f435df067e0b0f5a3cf3c0ecd5b50aeb10380b1c
Jake Partusch <jakepartusch@gmail.com>
2018-10-07 16:04:53 -0500

    feat: Handle 401 errors more gracefully

--- src/lambda/powerRankings.js
+++ src/lambda/powerRankings.js
@@ -17,6 +17,12 @@ export const handler = async event => {
             body: JSON.stringify(results)
         }
     } catch(e) {
+        if(e && e.response && e.response.status === 401) {
+            return {
+                statusCode: 401,
+                body: JSON.stringify({errorMessage: "Unable to retrieve private league power rankings"})
+            }
+        }
         console.error("Unable to retrieve power rankings", e);
         return {
             statusCode: 500,
