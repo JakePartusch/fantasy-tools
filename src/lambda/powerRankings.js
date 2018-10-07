c1d6abc88f947a0aa4df2eb1781263f0fc9435b7
Jake Partusch <jakepartusch@gmail.com>
2018-10-07 15:51:31 -0500

    feat: Better error handling and logging

--- src/lambda/powerRankings.js
+++ src/lambda/powerRankings.js
@@ -1,18 +1,26 @@
 import { FantasyFootballApi } from '../api/FantasyFootballApi';
 
-export const handler = async(event, context, callback) => {
-    console.log(event)
-    const { queryStringParameters } = event;
-    if(!queryStringParameters) {
+export const handler = async event => {
+    try {
+        const { queryStringParameters } = event;
+        if(!queryStringParameters) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ errorMessage: "Invalid request parameters" })
+            }
+        }
+        const { leagueId, seasonId } = queryStringParameters;
+        const api = new FantasyFootballApi();
+        const results = await api.getPowerRankings(leagueId, seasonId)
         return {
-            statusCode: 400,
+            statusCode: 200,
+            body: JSON.stringify(results)
+        }
+    } catch(e) {
+        console.error("Unable to retrieve power rankings", e);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({errorMessage: "Unable to retrieve power rankings"})
         }
     }
-    const { leagueId, seasonId } = queryStringParameters;
-    const api = new FantasyFootballApi();
-    const results = await api.getPowerRankings(leagueId, seasonId)
-    return {
-        statusCode: 200,
-        body: JSON.stringify(results)
-      }
   }
