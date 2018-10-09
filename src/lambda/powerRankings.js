b67e8d06030d22cab19b697c9955ddf9eafcda5e
Jake Partusch <jakepartusch@gmail.com>
2018-10-08 20:48:37 -0500

    feat: Add serverless function support for login (#59)

--- src/lambda/powerRankings.js
+++ src/lambda/powerRankings.js
@@ -2,15 +2,14 @@ import { FantasyFootballApi } from '../api/FantasyFootballApi';
 
 export const handler = async event => {
     try {
-        const { queryStringParameters } = event;
-        if(!queryStringParameters) {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({ errorMessage: "Invalid request parameters" })
-            }
-        }
-        const { leagueId, seasonId } = queryStringParameters;
+        const body = JSON.parse(event.body);
+        const { s2, swid, leagueId, seasonId } = body;
         const api = new FantasyFootballApi();
+        
+        if(s2 && swid) {
+            const cookies = `espn_s2=${s2}; SWID=${swid};`
+            api.addCookies(cookies);
+        }
         const results = await api.getPowerRankings(leagueId, seasonId)
         return {
             statusCode: 200,
