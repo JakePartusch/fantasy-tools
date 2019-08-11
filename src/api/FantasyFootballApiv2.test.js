ecd348b8b5434e93fac3d17009c52ee75447421f
Jacob Partusch <jakepartusch@gmail.com>
2019-08-11 09:40:37 -0500

    feat: fixing tests

--- src/api/FantasyFootballApiv2.test.js
+++ src/api/FantasyFootballApiv2.test.js
@@ -1,7 +1,17 @@
 import { getPowerRankings } from './FantasyFootballApiv2';
+import axios from 'axios';
+import response from './response.json';
+
+jest.mock('axios', () => ({
+  get: jest.fn(),
+  defaults: {}
+}));
+
+jest.mock('@3846masa/axios-cookiejar-support');
 
 describe('API', () => {
   it('should calculate the rankings', async () => {
+    axios.get.mockReturnValueOnce(Promise.resolve({ data: response }));
     const rankings = await getPowerRankings();
     expect(rankings[0].totalWins).toEqual(88);
     expect(rankings[0].totalLosses).toEqual(44);
