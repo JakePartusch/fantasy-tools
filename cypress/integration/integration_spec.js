e9fa75cb21f4081ecec79f79dc91e03371334cfc
Jacob Partusch <jakepartusch@gmail.com>
2019-10-08 21:30:06 -0500

    fix: remove hardcoded urls

--- cypress/integration/integration_spec.js
+++ cypress/integration/integration_spec.js
@@ -1,6 +1,6 @@
 describe('Test basic functionality', function() {
   it('visits the app', function() {
-    cy.visit('http://localhost:3000');
+    cy.visit('/');
     cy.contains('app');
     cy.get('header').contains('Fantasy Tools');
   });
@@ -10,17 +10,17 @@ describe('Test basic functionality', function() {
       cy.route('GET', 'https://fantasy.espn.com/apis/v3/**','fixture:response.json')
     })
     it('navigates to ranking from button click', () => {
-      cy.visit('http://localhost:3000');
+      cy.visit('/');
       cy.get('.rankings-simulator-btn').click();
     });
     it('loads team data', () => {
-      cy.visit('http://localhost:3000/rankings');
+      cy.visit('/rankings');
       cy.get('input').type('https://fantasy.espn.com/football/league?leagueId=452354');
       cy.get('form').submit();
       cy.get('tr').should('have.length', 13)
     });
     it('should rank the teams correctly', () => {
-      cy.visit('http://localhost:3000/rankings');
+      cy.visit('/rankings');
       cy.get('input').type('https://fantasy.espn.com/football/league?leagueId=452354');
       cy.get('form').submit();
       cy.get('tbody tr').first().should('contain', 'Grover Circle Clevelands')
