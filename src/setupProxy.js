f7ea4f2a5a436757e68820a9d8fb1b1237bd70a1
Jacob Partusch <jakepartusch@gmail.com>
2019-10-27 19:53:30 -0500

    feat: creating and fetching user data with auth0

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,10 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
+    proxy('/dev/user', {
+      target: 'https://7kzk3mcrg1.execute-api.us-east-1.amazonaws.com',
+      changeOrigin: true
+    }),
     proxy('/dev/user/email', {
       target: 'https://7kzk3mcrg1.execute-api.us-east-1.amazonaws.com',
       changeOrigin: true
