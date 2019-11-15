4141ed00128fd858f2c1275271fe73296c4b3d66
Jake Partusch <jakepartusch@gmail.com>
2019-11-14 22:21:20 -0600

    Feature/ux (#133)
    * fix: adjusting mobile padding (#123)
    
    * Update cypress to the latest version 🚀 (#125)
    
    * chore(package): update cypress to version 3.6.0
    
    * chore(package): update lockfile package-lock.json
    
    * fix: bump up maxWidth so table cells don't wrap on desktop (#128)
    
    * Update cypress to the latest version 🚀 (#129)
    
    * chore(package): update cypress to version 3.6.1
    
    * chore(package): update lockfile package-lock.json
    
    * feat: full page loader, library upgrades
    
    * feat: improving the loading interaction for the standings

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -62,10 +62,12 @@ const Home = () => {
             Get Started!
           </Button>
         </div>
-        <img css={{ maxWidth: '300px' }} alt="Football fans on coach" src={FansImg} />
+        <div css={{ width: 300, height: 270 }}>
+          <img css={{ maxWidth: '100%' }} alt="Football fans on coach" src={FansImg} />
+        </div>
       </header>
       <section css={{ maxWidth: 960, margin: '3rem auto 0 auto' }}>
-        <Grid container spacing={2} justify={'center'}>
+        <Grid container spacing={2} justify={'center'} css={{ width: '100%' }}>
           <Grid item md={4} xs={10}>
             <Button
               className="rankings-simulator-btn"
