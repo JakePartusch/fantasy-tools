f34053ff00804b4fece9b654ec824bebe0b58704
Jake Partusch <jakepartusch@gmail.com>
2019-11-28 21:07:27 -0600

    fix: adding retry for expired/invalid sessions (#143)

--- api/fantasytools-api/src/league/fetchLeague.js
+++ api/fantasytools-api/src/league/fetchLeague.js
@@ -1,15 +1,23 @@
 import axios from 'axios';
 import { getSyncedAccountCookiesByEmail } from '../authentication';
 
-const fetchLeague = async (email, leagueId, seasonId = '2019') => {
-  const { s2, swid } = await getSyncedAccountCookiesByEmail(email);
+const fetchLeague = async (email, leagueId, seasonId = '2019', forceRefresh = false) => {
+  const { s2, swid } = await getSyncedAccountCookiesByEmail(email, forceRefresh);
   const cookieString = `espn_s2=${s2}; swid=${swid}`;
-  const leagueResponse = await axios.get(
-    `https://fantasy.espn.com/apis/v3/games/ffl/seasons/${seasonId}/segments/0/leagues/${leagueId}?view=mMatchupScore&view=mStatus&view=mSettings&view=mTeam&view=modular&view=mNav`,
-    { headers: { Cookie: cookieString } }
-  );
-  console.log(JSON.stringify(leagueResponse.data));
-  return leagueResponse.data;
+  try {
+    const leagueResponse = await axios.get(
+      `https://fantasy.espn.com/apis/v3/games/ffl/seasons/${seasonId}/segments/0/leagues/${leagueId}?view=mMatchupScore&view=mStatus&view=mSettings&view=mTeam&view=modular&view=mNav`,
+      { headers: { Cookie: cookieString } }
+    );
+    console.log(JSON.stringify(leagueResponse.data));
+    return leagueResponse.data;
+  } catch (error) {
+    if (error.response && error.response.status === 401 && !forceRefresh) {
+      const league = await fetchLeague(email, leagueId, seasonId, true);
+      return league;
+    }
+    throw error;
+  }
 };
 
 module.exports.fetchLeague = async event => {
